5cd99a27d749c833da698e8f7a96950fb8bc0b77
Daemondeal <daemondeal@gmail.com>
2022-12-18 19:09:36 +0100

    2022/day17

--- 2022/day17/src/main.rs
+++ 2022/day17/src/main.rs
@@ -5,6 +5,7 @@ use std::thread::sleep;
 use std::time::Duration;
 
 use lazy_static::lazy_static;
+use tqdm::Iter;
 
 #[derive(Debug)]
 enum MoveDirection {
@@ -41,9 +42,11 @@ fn process_input(input: &str) -> Vec<MoveDirection> {
         .collect()
 }
 
+#[derive(Clone)]
 struct TetrisMap {
     layers: Vec<[bool; 7]>,
     current_shape: usize,
+    bottom_height: usize,
 }
 
 impl Display for TetrisMap {
@@ -89,6 +92,7 @@ impl TetrisMap {
         Self {
             layers: vec![],
             current_shape: 0,
+            bottom_height: 0,
         }
     }
 
@@ -107,27 +111,41 @@ impl TetrisMap {
         for (i, piece_layer) in shape.iter().enumerate() {
             for (j, cell) in piece_layer.iter().enumerate() {
                 let (x, y) = (x as usize, y as usize);
-                self.layers[y + (height - i - 1)][x + j] = *cell;
+                if *cell == true {
+                    self.layers[y + (height - i - 1)][x + j] = *cell;
+                }
             }
         }
     }
 
     fn check_collision(&self, x: i64, y: i64, shape: &[Vec<bool>]) -> bool {
-        let height = shape.len();
+        let height = shape.len() as i64;
         for (i, piece_layer) in shape.iter().enumerate() {
             for (j, cell) in piece_layer.iter().enumerate() {
-                if x + j as i64 >= 7 || x + (j as i64) < 0 || y < 0 {
+                if cell == &false {
+                    continue;
+                }
+
+                let x_pos = x + j as i64;
+                let y_pos = y + (height - i as i64 - 1);
+
+                if x_pos >= 7 || x_pos < 0 || y_pos < 0 {
                     return true;
                 }
-                let (x, y) = (x as usize, y as usize);
 
-                if self.layers[y + (height - i - 1)][x + j] == true && cell == &true {
+                if self.layers[y_pos as usize][x_pos as usize] == true {
                     return true;
                 }
             }
         }
 
         false
+    }
+
+    fn print_with_shape(&self, x: i64, y: i64, shape: &[Vec<bool>]) {
+        let mut new_map = (*self).clone();
+        new_map.insert_shape(x, y, shape);
+        println!("{new_map}");
     }
 
     pub fn next_shape(&mut self, moves: &[MoveDirection], last_move: usize) -> usize {
@@ -146,7 +164,17 @@ impl TetrisMap {
         let mut y = rocks_height as i64 + 3;
         let mut move_index = last_move;
 
+        // 100 should be big enough
+        let upper_height_limit = 100;
+
         loop {
+            while self.layers.len() > upper_height_limit {
+                self.layers.remove(0);
+                self.bottom_height += 1;
+
+                y -= 1;
+            }
+
             let next_move = &moves[move_index];
             move_index = (move_index + 1) % moves.len();
 
@@ -176,19 +204,61 @@ impl TetrisMap {
 
 fn solve_a(input: &str) -> usize {
     let moves = process_input(input);
-    println!("{}", moves.len());
     let mut map = TetrisMap::new();
 
     let mut move_index = 0;
 
     for _ in 0..2022 {
-        print!("{}[2J", 27 as char);
         move_index = map.next_shape(&moves, move_index);
     }
 
-    map.get_height()
+    map.get_height() + map.bottom_height
 }
 
-fn solve_b(input: &str) -> i32 {
-    0
+fn solve_b(input: &str) -> usize {
+    let moves = process_input(input);
+    let mut map = TetrisMap::new();
+
+    let mut move_index = 0;
+
+    let mut snapshot = TetrisMap::new();
+    let mut snap_len = 0;
+    let mut snap_move_index = 0;
+
+    let total_iters = 1000000000000i64;
+
+    // Needs to be a big number, to let the cyclicity settle in
+    let starting_snap = 10000;
+
+    let mut remaining_iters = total_iters;
+    let mut skipped = 0;
+
+    while remaining_iters > 0 {
+        let iter = total_iters - remaining_iters;
+        move_index = map.next_shape(&moves, move_index);
+
+        if iter == starting_snap {
+            snapshot = map.clone();
+            snap_len = snapshot.get_height() + snapshot.bottom_height;
+            snap_move_index = move_index;
+        } else if iter > starting_snap
+            && skipped == 0
+            && snapshot.layers == map.layers
+            && map.current_shape == snapshot.current_shape
+            && move_index == snap_move_index
+        {
+            // Cycle found
+
+            let delta = map.get_height() + map.bottom_height - snap_len;
+            let cycle_length = iter - starting_snap;
+            let cycles_skipped =
+                ((remaining_iters) as f64 / (cycle_length as f64)).floor() as usize;
+
+            skipped = delta * cycles_skipped;
+            remaining_iters -= cycles_skipped as i64 * cycle_length;
+        }
+        remaining_iters -= 1;
+    }
+
+    map.get_height() + map.bottom_height + skipped
 }
