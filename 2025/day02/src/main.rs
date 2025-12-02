781a27b5ce8eeb21e7e53161c7679f643f05c709
Daemondeal <daemondaeal@gmail.com>
2025-12-02 23:56:49 +0100

    2025 day02 done

--- 2025/day02/src/main.rs
+++ 2025/day02/src/main.rs
@@ -15,11 +15,11 @@ fn main() {
     println!("B: {}", solve_b(&input));
 }
 
-fn process_input(input: &str) -> Vec<(i32, i32)> {
+fn process_input(input: &str) -> Vec<(i64, i64)> {
     input
         .split(",")
         .map(|x| {
-            let z: Vec<i32> = x.split("-").map(|y| y.parse::<i32>()).flatten().collect();
+            let z: Vec<i64> = x.trim().split("-").map(|y| y.parse::<i64>()).flatten().collect();
             if z.len() == 2 {
                 Some((z[0], z[1]))
             } else {
@@ -30,8 +30,8 @@ fn process_input(input: &str) -> Vec<(i32, i32)> {
         .collect()
 }
 
-fn clog10(x: i32) -> i32 {
-    (x as f32).log10().ceil() as i32
+fn clog10(x: i64) -> i64 {
+    (x as f64).log10().ceil() as i64
 }
 
 fn solve_a(input: &str) -> i64 {
@@ -43,9 +43,9 @@ fn solve_a(input: &str) -> i64 {
             let len = clog10(i) as u32;
 
             if len % 2 == 0 {
-                let base = i % (10_i32.pow(len/2));
+                let base = i % (10_i64.pow(len/2));
 
-                if ((base * 10_i32.pow(len/2)) + base) == i {
+                if ((base * 10_i64.pow(len/2)) + base) == i {
                     count += i as i64;
                 }
             }
@@ -56,6 +56,33 @@ fn solve_a(input: &str) -> i64 {
     count
 }
 
-fn solve_b(input: &str) -> i32 {
-    0
+fn solve_b(input: &str) -> i64 {
+    let ranges = process_input(input);
+
+    let mut count: i64 = 0;
+    for (l, r) in ranges {
+        for i in l..=r {
+            let len = clog10(i) as u32;
+
+            for j in 2..=len {
+                if len % j == 0 {
+                    let expon = 10_i64.pow(len/j);
+
+                    let base = i % expon;
+
+                    let mut acc = 0;
+                    for _ in 0..j {
+                        acc *= expon;
+                        acc += base;
+                    }
+                    if acc == i {
+                        count += i as i64;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    count
 }
