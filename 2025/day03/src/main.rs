bf7bee1082f3a8cf7ce5215b1120770dbbcd6b5f
Pietro di Maria <pietro.dimaria@arm.com>
2025-12-03 09:14:34 +0100

    2025 day03 done

--- 2025/day03/src/main.rs
+++ 2025/day03/src/main.rs
@@ -1,6 +1,9 @@
 use std::env;
 use std::fs;
 
+use rayon::iter::IntoParallelRefIterator;
+use rayon::iter::ParallelIterator;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -51,33 +54,28 @@ fn max_joltage_2(bank: &[u64]) -> u64 {
     cur_max
 }
 
-fn max_jolt(bank: &[u64], i: u64, n: u64) -> u64 {
-    if (i+n) >= bank.len() as u64 {
-        return 0;
+fn max_jolt(bank: &[u64], i: usize, n: usize, cur: u64) -> u64 {
+    let mut cur_max: u64 = 0;
+    if n == 0 {
+        return cur;
     }
 
-
-    1
-}
-
-fn max_joltage_12(bank: &[u64]) -> u64 {
-    let mut cur_max = 0;
-
-    for i in 0..bank.len()-1 {
-        if cur_max > (bank[i] * 10 + 9) {
-            continue;
-        }
-
-        for j in (i+1)..bank.len() {
-            let n = bank[i] * 10 + bank[j];
-            if n > cur_max {
-                cur_max = n;
+    let mut cur_digit_max: u64 = 0; 
+    for j in i..bank.len() {
+        if j+n <= bank.len() && bank[j] > cur_digit_max {
+            cur_digit_max = bank[j];
+            let tot = max_jolt(bank, j+1, n-1, cur*10+bank[j]);
+            if tot > cur_max {
+                cur_max = tot;
             }
         }
     }
 
+    return cur_max;
+}
 
-    cur_max
+fn max_joltage_12(bank: &[u64]) -> u64 {
+    max_jolt(bank, 0, 12, 0)
 }
 
 fn solve_a(input: &str) -> i64 {
@@ -89,7 +87,7 @@ fn solve_a(input: &str) -> i64 {
 
 fn solve_b(input: &str) -> i64 {
     process_input(input)
-        .iter()
-        .map(|x| max_joltage_2(x) as i64)
+        .par_iter()
+        .map(|x| max_joltage_12(x) as i64)
         .sum()
 }
