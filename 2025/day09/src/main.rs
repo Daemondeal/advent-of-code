e66b710d8fbce1c8267910d484fb4cc788bfafd3
Daemondeal <daemondaeal@gmail.com>
2025-12-10 00:06:10 +0100

    optimized 2025 day09

--- 2025/day09/src/main.rs
+++ 2025/day09/src/main.rs
@@ -16,7 +16,7 @@ fn main() {
     println!("B: {}", solve_b(&input));
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Point {
     x: i64,
     y: i64,
@@ -26,6 +26,37 @@ impl Display for Point {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "({}, {})", self.x, self.y)
     }
+}
+
+
+// Liang-Barsky
+// https://en.wikipedia.org/wiki/Liang%E2%80%93Barsky_algorithm
+fn line_intersects_box(p0: Point, p1: Point, xmin: i64, xmax: i64, ymin: i64, ymax: i64) -> bool {
+    let dx = p1.x - p0.x;
+    let dy = p1.y - p0.y;
+
+    let mut u1: f64 = 0.0;
+    let mut u2: f64 = 1.0;
+
+    for (pi, qi) in [
+        (-dx, p0.x - xmin),
+        (dx, xmax - p0.x),
+        (-dy, p0.y - ymin),
+        (dy, ymax - p0.y),
+    ] {
+        if pi == 0 {
+            if qi < 0 {
+                return false;
+            }
+        } else if pi < 0 {
+            u1 = ((qi as f64)/(pi as f64)).max(u1);
+        } else if pi > 0 {
+            u2 = ((qi as f64)/(pi as f64)).min(u2);
+        }
+
+    }
+
+    u1 <= u2
 }
 
 fn process_input(input: &str) -> Vec<Point> {
@@ -64,33 +95,6 @@ fn solve_a(input: &str) -> i64 {
 fn solve_b(input: &str) -> i64 {
     let points = process_input(input);
 
-    let mut perimeter = vec![];
-
-    for i in 0..points.len() {
-        let start = if i == 0 {
-            &points[points.len() - 1]
-        } else {
-            &points[i - 1]
-        };
-        let end = &points[i];
-
-        if start.x == end.x {
-            let y_start = start.y.min(end.y);
-            let y_end = start.y.max(end.y);
-
-            for y in y_start..y_end {
-                perimeter.push(Point { x: start.x, y });
-            }
-        } else {
-            let x_start = start.x.min(end.x);
-            let x_end = start.x.max(end.x);
-
-            for x in x_start..x_end {
-                perimeter.push(Point { x, y: start.y });
-            }
-        }
-    }
-
     let mut area_max = 0;
     for (i, px) in points.iter().enumerate() {
         'outer: for (j, py) in points.iter().enumerate() {
@@ -107,8 +111,15 @@ fn solve_b(input: &str) -> i64 {
                 let ymin = px.y.min(py.y);
                 let ymax = px.y.max(py.y);
 
-                for p in perimeter.iter() {
-                    if p.x > xmin && p.x < xmax && p.y > ymin && p.y < ymax {
+                for i in 0..points.len() {
+                    let start = if i == 0 {
+                        &points[points.len() - 1]
+                    } else {
+                        &points[i - 1]
+                    };
+                    let end = &points[i];
+
+                    if line_intersects_box(*start, *end, xmin + 1, xmax - 1, ymin + 1, ymax - 1) {
                         continue 'outer;
                     }
                 }
@@ -118,22 +129,6 @@ fn solve_b(input: &str) -> i64 {
         }
     }
 
-    // let mut bitmap = [0_u8; 15 * 15];
-    // for p in perimeter.iter() {
-    //     bitmap[(p.x + p.y * 15) as usize] = 255;
-    // }
-    // for x in 0..15 {
-    //     for y in 0..15 {
-    //         let r = bitmap[x + y * 15];
-    //         print!("\x1b[38;2;{r};0;0m█");
-    //     }
-    //     println!();
-    // }
-    // println!("\x1b[0m");
-
     area_max
 }
 
-fn pixel(r: u8, g: u8, b: u8) {
-    print!("\x1b[38;2;{r};{g};{b}m█");
-}
